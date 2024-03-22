6404ecb1846a621611c94f129bc3ac5aabb41576
joshbrod <joshbrod@umich.edu>
2024-03-21 21:06:26 -0400

    basic UI and started working with api endpoints

--- src/app/api/sessionDB/connect/route.ts
+++ src/app/api/sessionDB/connect/route.ts
@@ -1,1 +1,17 @@
 /* API endpoint for connecting to a session */
+import { redirect } from 'next/navigation'
+import { connectToSession } from '../../../../database/db'
+
+export async function POST(req: Request) {
+    const data = await req.json();
+    const guestCode : string = data.guestCode;
+
+    // TODO: Create database handler that verifies unique guestCode exists in an existing session
+
+    // TODO: Create database handler that sends user information to database to be added to session
+    
+    const sessionData = await connectToSession(guestCode);
+    
+    // If passes all checks, redirect to session page
+    redirect('/session');
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,56 @@
+'use client'
 import Image from "next/image";
-import { useState }  from 'react';
+import { useState, useEffect } from 'react';
+
 
 export default function Home() {
+  const [guestCode, setGuestCode] = useState("");
+  
+  useEffect(() => {
+    // This effect will run only on the client side
+    // You can place any client-side specific logic here
+  }, []); // Empty dependency array ensures the effect runs only once on mount
+
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p>Welcome to [insert name here]</p>
-
-      <div>
-        <div>
-            <button>I'm a host</button>
-            </div>
-        <div>
-            <button>I'm a guest</button>
+      <img src="GMJ-emblem-color.svg" alt="" />
+      <div className="options">
+        <div className="hostoptions">
+            <h1>I'm a host:</h1>
+            <button className="SubmitButton"> Host </button>
+        </div>
+        <div className="divideDiv">
+        <hr className="divider"></hr>
+        </div>
+        
+        <div className="guestoptions">
+            <h1>I'm a guest:</h1>
+            <form data-testid="comment-form" onSubmit={(e) => {e.preventDefault(); connectToSession(guestCode);} }>
+                <input type="text" maxLength={6} name="guestcode" value={guestCode} onChange={(e) => setGuestCode(e.target.value.toUpperCase())}/>
+            </form>
+            <button className="SubmitButton" onClick={() => {connectToSession(guestCode)}}>Join</button>
         </div>
       </div>
     </main>
   );
+}
+
+async function connectToSession(guestCode : string) : Promise<void> {
+  console.log(guestCode);  
+  try {
+     await fetch('api/sessionDB/connect', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+         guestCode: guestCode
+      }),
+    });
+  }
+  catch(e){
+    // Add some error message to user saying that wrong code was entered
+  }
 }
