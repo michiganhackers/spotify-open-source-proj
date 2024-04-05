88f82cdf3abab3f3adf41e11a1bb8f2cfd6c5daf
Jadon Yack <jyack@umich.edu>
2024-04-04 20:28:47 -0400

    Add database endpoint to create new session.

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,9 +1,9 @@
-import postgres from 'postgres';
+import postgres from 'postgres'
 import 'dotenv/config'
 
 const sql = postgres(process.env.POSTGRES_URI!) // will use psql environment variables
 
-export async function connectToSession(guestCode : string) : Promise<any> {
+export async function ConnectToSession(guestCode : string) : Promise<any> {
     const session : any[] = await sql`
         SELECT session FROM sessions
         WHERE session.session_id = ${guestCode}
@@ -14,4 +14,49 @@ export async function connectToSession(guestCode : string) : Promise<any> {
     }
 
     return session[0]; // Return the session as an object (maybe only need to return the session_id)
+}
+
+export async function CreateSession(
+    hostID : number,
+    accessToken : string,
+    refreshToken : string) : Promise<any> {
+
+    // Get all currently used session codes
+    const codes : any[] = await sql`
+        SELECT session_id FROM sessions
+    `
+
+    function CreateCode() {
+        // Generates a new code
+        const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        let result = "";
+        for (let i = 0; i < 8; i++) {
+            result += chars.charAt(Math.floor(Math.random() * chars.length));
+        }
+        return result;
+    }
+
+    // Generate a new code until it is unique
+    let code;
+    let is_unique = true
+    do {
+        code = CreateCode();
+        for (let i = 0; i < codes.length; i++) {
+            if (code === codes[i]['session_id']) is_unique = false;
+        }
+    } while (!is_unique)
+
+    // Insert new session into DB
+    const session = {
+        "session_id": code,
+        "host_id": hostID,
+        "access_token": accessToken,
+        "refresh_token": refreshToken
+    }
+    await sql`
+    INSERT INTO sessions
+    VALUES (${sql(session)})
+    `
+
+    return code;
 }
