14a0ec11ab2c0b6959920180d5fc3271e2ed747b
joshbrod <joshbrod@umich.edu>
2025-03-13 21:19:39 -0400

    Fixed favicon and title metadata, and added a screen when a guest is kicked from a session

--- src/socket/client.ts
+++ src/socket/client.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 import 'dotenv/config'
 
-const SERVER = `ws://${process.env.PROD_SERVER_IP}/ws/`; // LATER: remove and add as env variable
+const SERVER = process.env.NEXT_PUBLIC_WS_SERVER; // LATER: remove and add as env variable
 
 export function socketIO(sid : string, isHost : boolean) {
     return io(SERVER, {
